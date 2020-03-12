23dc5c4801c5c01c6858130fbc238a73479ce82a
Garrett <garbwalk@gmail.com>
2020-03-11 20:00:39 -0500

    Starting on the frontend
    modified:   package.json
    * Installed buefy, it's a vue component library based on bulma
    
    modified:   public/css/app.css
    modified:   public/js/app.js
    new file:   public/js/app.js.LICENSE.txt
    * Should these be commited?
    
    modified:   resources/js/app.js
    * Imported buefy, added mock data to root vue instance
    
    modified:   resources/sass/app.scss
    * import bulma/buefy styles
    
    new file:   resources/sass/styles.scss
    * lets make it look pretty
    
    modified:   resources/views/welcome.blade.php
    * Started on layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,59 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue');
+var Vue = require("vue");
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+const files = require.context("./", true, /\.vue$/i);
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split("/")
+      .pop()
+      .split(".")[0],
+    files(key).default
+  )
+);
 
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Buefy from "buefy";
+Vue.use(Buefy);
 
 const app = new Vue({
-    el: '#app',
+  el: "#app",
+  data() {
+    return {
+      data: [
+        {
+          clockedIn: "2016-10-15 13:43:27",
+          clockedOut: "2016-10-15 18:43:27"
+        },
+        {
+          clockedIn: "2016-10-15 18:43:27",
+          clockedOut: "2016-04-26 06:26:28"
+        },
+        {
+          clockedIn: "2016-04-26 06:26:28",
+          clockedOut: "2016-04-26 019:26:28"
+        },
+        {
+          clockedIn: "2016-04-26 019:26:28",
+          clockedOut: "2016-12-06 14:38:38"
+        },
+        {
+          clockedIn: "2016-12-06 14:38:38",
+          clockedOut: "2016-12-06 14:38:38"
+        }
+      ],
+      columns: [
+        {
+          field: "clockedIn",
+          label: "In",
+          centered: true
+        },
+        {
+          field: "clockedOut",
+          label: "Out",
+          centered: true
+        }
+      ]
+    };
+  }
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-      
-  </head>
-  <body>
-    <div id="app">
-        <example-component></example-component>
-        <current-time></current-time>
-        asd
-    </div>
 
-    <!-- Scripts -->
-    <script src="/js/app.js" ></script>
-  </body>
+<head>
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+  <div id="app">
+    <section class="hero is-success is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="column is-4 is-offset-4">
+            <h3 class="title has-text-white">TimeClock</h3>
+            <hr class="title-hr" />
+            <p class="subtitle has-text-white">
+              <current-time @bla="$emit('asd')"/>
+            </p>
+            <div class="box">
+              <div class="entry-table">
+                <b-table :data="data" :columns="columns" sticky-header></b-table>
+              </div>
+            </div>
+            <button class="button is-block is-warning is-large is-fullwidth">
+              Clock In <i class="fa fa-sign-in" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+  <script>
+
+  </script>
+</body>
+
 </html>
